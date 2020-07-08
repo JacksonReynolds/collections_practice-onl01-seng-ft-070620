4277dc4fcf0bba95155673c67ce9928ec75f5e04
Jackson Reynolds <jwreynold@gmail.com>
2020-07-08 19:29:55 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -25,7 +25,7 @@ def reverse_array(ints)
 end #reverse_array
 
 def kesha_maker(stgs)
-  stgs.each.collect {|word| word.sub('s','$')}
+  stgs.each.collect {|word| word[0..1]+'$'+word[3..-1] }
 end #kesha_maker
 
 def find_a(stgs)
