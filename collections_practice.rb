875467e39f6d5f9d0299fe132a8315dc9c252c74
Jackson Reynolds <jwreynold@gmail.com>
2020-07-08 18:41:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,39 @@
+def sort_array_asc(ints)
 
+end #sort_array_asc
+
+def sort_array_desc(ints)
+
+end #sort_array_desc
+
+def sort_array_char_count(stgs)
+
+end #sort_array_char_count
+
+def swap_elements(arr)
+
+end #swap_elements
+
+def swap_elements_from_to(arr, ind_1, ind_2)
+
+end #swap_elements_from_to
+
+def reverse_array(ints)
+
+end #reverse_array
+
+def kesha_maker(stgs)
+
+end #kesha_maker
+
+def find_a(stgs)
+
+end #find_a
+
+def sum_array(ints)
+
+end #sum_array
+
+def add_s(stgs)
+
+end #add_s
